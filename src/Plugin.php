41f41d514f1fbd2ca4ea964562e513e33bec5e38
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-08-05 20:59:42 +0530

    fix: add Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,1 +1,90 @@
 <?php
+namespace MG\Give\Coupons;
+
+use MG\Give\Coupons\Includes;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Loads and registers plugin functionality through WordPress hooks.
+ *
+ * @since 1.0.0
+ */
+final class Plugin {
+
+	/**
+	 * Registers functionality with WordPress hooks.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function register() {
+		// Handle plugin activation and deactivation.
+		register_activation_hook( COUPONS4GIVE_PLUGIN_FILE, array( $this, 'activate' ) );
+		register_deactivation_hook( COUPONS4GIVE_PLUGIN_FILE, array( $this, 'deactivate' ) );
+
+		// Register services used throughout the plugin.
+		add_action( 'plugins_loaded', array( $this, 'register_services' ) );
+
+		// Load text domain.
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+	}
+
+	/**
+	 * Registers the individual services of the plugin.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function register_services() {
+		new Includes\Filters();
+
+		new Includes\Actions();
+	}
+
+	/**
+	 * Loads the plugin's translated strings.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain(
+			'coupons-for-give',
+			false,
+			dirname( plugin_basename( COUPONS4GIVE_PLUGIN_FILE ) ) . '/languages/'
+		);
+	}
+
+	/**
+	 * Handles activation procedures during installation and updates.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @param bool $network_wide Optional. Whether the plugin is being enabled on
+	 *                           all network sites or a single site. Default false.
+	 *
+	 * @return void
+	 */
+	public function activate( $network_wide = false ) {}
+
+	/**
+	 * Handles deactivation procedures.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 *
+	 * @return void
+	 */
+	public function deactivate() {}
+}
