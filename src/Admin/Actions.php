dd23a783620c9154db6021000849e17d8bf0a7f4
Mehul Gohil <mehul.gohil0810@gmail.com>
2020-08-09 17:04:27 +0530

    fix: add separate page to handle importing coupon code via csv

--- src/Admin/Actions.php
+++ src/Admin/Actions.php
@@ -30,8 +30,8 @@ class Actions {
      * @return void
 	 */
 	public function __construct() {
+	    add_action( 'admin_menu', [ $this, 'registerImportSubMenu' ] );
 		add_action( 'manage_mvnm_coupon_posts_custom_column', [ $this, 'addAmountColumnData' ], 10, 2);
-		add_action( 'give_tools_tab_import_table_bottom', [ $this, 'importCouponCodeScreen' ] );
 	}
 
 	/**
@@ -47,7 +47,7 @@ class Actions {
 	 */
 	public function addAmountColumnData( $column, $postId ) {
 		if ( 'amount' === $column ) {
-			$couponAmount = give_maybe_sanitize_amount( get_post_meta( $postId, '_coupon_for_give_amount', true ) );
+			$couponAmount = give_maybe_sanitize_amount( get_post_meta( $postId, '_coupons_for_give_amount', true ) );
 
 			echo give_currency_filter(
                 give_format_amount(
@@ -65,28 +65,71 @@ class Actions {
     }
 
 	/**
-	 * Import Coupon Code Screen.
+	 * Import Submenu
+	 */
+	public function registerImportSubMenu() {
+		add_submenu_page(
+            'edit.php?post_type=mvnm_coupon',
+			esc_html__( 'Import Coupons', 'coupons-for-give' ),
+			esc_html__( 'Import Coupons', 'coupons-for-give' ),
+			'manage_options',
+			'import_coupons',
+			[ $this, 'importPage' ]
+		);
+	}
+
+	/**
+	 * Import Page.
      *
-     * @since  1.0.0
-     * @access public
+     * @since 1.0.0
      *
      * @return void
 	 */
-	public function importCouponCodeScreen() {
-		?>
-		<tr class="give-import-coupons">
-			<td scope="row" class="row-title">
-				<h3>
-					<span><?php esc_html_e( 'Import Coupons', 'coupons-for-give' ); ?></span>
-				</h3>
-				<p><?php esc_html_e( 'Import a CSV of Coupons.', 'coupons-for-give' ); ?></p>
-			</td>
-			<td>
-				<a class="button" href="<?php echo add_query_arg( array( 'importer-type' => 'import_coupons' ) ); ?>">
-					<?php esc_html_e( 'Import CSV', 'coupons-for-give' ); ?>
-				</a>
-			</td>
-		</tr>
-		<?php
+	public function importPage() {
+		if ( isset( $_POST['submit'] ) ) {
+			$csv_file     = $_FILES['csv_file'];
+			$csv_to_array = array_map( 'str_getcsv', file( $csv_file['tmp_name'] ) );
+
+			foreach ( $csv_to_array as $key => $value ) {
+				if ( 0 === $key ) {
+					continue;
+				}
+
+				$value = array_filter( $value );
+
+				$insertId = wp_insert_post(
+                    [
+                        'post_title'  => $value[0],
+                        'meta_input'  => [
+                            '_coupons_for_give_amount' => (float) $value[1],
+                        ],
+                        'post_type'   => 'mvnm_coupon',
+                        'post_status' => 'publish',
+                    ]
+                );
+
+				echo "{$value[0]} has been inserted with ID #{$insertId} <br/>";
+			}
+		} else {
+            ?>
+            <div class="wrap">
+                <h1><?php esc_html_e( 'Import Coupons', 'coupons-for-give' ); ?></h1>
+                <hr/>
+                <form method="post" enctype="multipart/form-data">
+                    <p>
+                        <label>
+                            <strong>
+                                <?php esc_html_e( 'Select CSV File', 'coupons-for-give' ); ?>
+                            </strong>
+                            <input type="file" name="csv_file"/>
+                        </label>
+                    </p>
+                    <p>
+                        <input class="button button-primary" type="submit" name="submit" value="<?php esc_html_e( 'Import Coupons', 'coupons-for-give' ); ?>"/>
+                    </p>
+                </form>
+            </div>
+            <?php
+        }
 	}
 }
